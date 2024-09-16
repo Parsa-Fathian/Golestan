3b30d938e435fa467d35a41bd498918a5a357b8f
Parsa Fathian <parsa.fathian@gmail.com>
2024-09-16 10:05:35 +0330

    13

--- obj/Debug/net8.0/Golestan.AssemblyInfo.cs
+++ obj/Debug/net8.0/Golestan.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Golestan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a30f927cdee382e377a7062a5951dee899dab66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd34e906f7ce02cef0a28247317340b553d2182a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Golestan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Golestan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
